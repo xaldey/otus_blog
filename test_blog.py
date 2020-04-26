3d21b96edd4106c43672192dc779cf250d1fce3e
xaldey <fguzkp25@gmail.com>
2020-04-27 00:04:03 +0300

    Добавил проверку наличия файла.

--- test_blog.py
+++ test_blog.py
@@ -5,14 +5,34 @@ import datetime
 import pathlib
 import pytest
 
-path = pathlib.Path(input('Введите адрес: '))
-print(path.exists())  # True
-print(path.is_file())  # True
+expected_db_path = 'myblog.db'
 
-# class TestBlog:
-#     def is_base_exists(self):
-#         """Проверка запуска теста"""
-#         assert 1 == 1
+path = pathlib.Path(input('Введите имя файла БД: '))
+print(path)
+print(expected_db_path)
+# print(path.is_file() and path.exists())
+# print(type(path), path)
+# print(type(expected_db_path), expected_db_path)
+
+if (path.is_file() and path.exists()):
+    print('Файл', path,'в наличии.')
+    if str(path) == str(expected_db_path):
+        print('И это правильный и рабочий файл! :)')
+    elif str(path) != str(expected_db_path):
+        print('Только вот программа о нем ничего не знает.')
+        print('Возможно он не был создан нативным инструментом.')
+    else:
+        print('Неясно, как ты сюда попал :)')
+else:
+    print('Файла с именем', path,'не обнаружено.')
+
+
+
+class TestBlog:
+    def is_base_exists(self):
+        """Проверка запуска теста"""
+
+        assert 1 == 1
 #
 #     def test_db_contains(self, args, expected_result):
 #         pass
@@ -26,3 +46,5 @@ print(path.is_file())  # True
 #     def test_is_tags_exsists(self, args):
 #         pass
 
+if __name__ == '__main__':
+    TestBlog
